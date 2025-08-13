653cd509797eb12e42068f1936c6d7f9d49e5104
ardinata2724 <39457874+ardinata2724@users.noreply.github.com>
2025-08-14 00:17:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -339,6 +339,7 @@ with st.sidebar:
     st.markdown("---")
     st.markdown("### 🎯 Opsi Prediksi")
     jumlah_digit = st.slider("🔢 Jumlah Digit Prediksi", 1, 9, 6)
+    jumlah_digit_shio = st.slider("🐉 Jumlah Digit Prediksi Khusus Shio", 1, 12, 12)
     metode = st.selectbox("🧠 Metode", ["Markov", "LSTM AI"])
     use_transformer = st.checkbox("🤖 Gunakan Transformer", value=True)
     model_type = "transformer" if use_transformer else "lstm"
